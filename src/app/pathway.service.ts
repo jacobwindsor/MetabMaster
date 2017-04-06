32ae4e961ab61fb64f51c46448627a3995727e7b
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-06 22:36:29 +0200

    Use a list of entries

--- src/app/pathway.service.ts
+++ src/app/pathway.service.ts
@@ -12,21 +12,24 @@ export class PathwayService {
    * @param WPId - The ID for the WikiPathways pathway. Exclude the 'WP'. E.g. use 78 for TCA cycle
    * @param values
    */
-  create(WPId: number, values: {description: string, userId: string}): Promise<any> {
-    return this.fb.db.ref('pathways/' + WPId).set(values);
+  create(values: {WPId: number, title: string, description: string, userId: string}): Promise<any> {
+    const ref = this.fb.db.ref('pathways').push();
+    return ref.set(values);
   }
 
   /**
    * Get a specific pathway.
-   * @param WPId
+   * @param id
    * @returns {Observable}
    */
-  get(WPId: number): Observable<{WPId: number, description: string, userId: string}> {
+  get(id: string): Observable<{id: string, WPId: number, title: string, description: string, userId: string}> {
     return Observable.fromPromise(new Promise((resolve, reject) => {
       this.fb.db.ref('pathways/' + WPId).once('value').then(snapshot => {
         const val = snapshot.val();
         resolve({
-          WPId: snapshot.key(),
+          id: snapshot.key(),
+          WPId: val.WPId,
+          title: val.title,
           description: val.description,
           userId: val.userId
         });
@@ -38,28 +41,28 @@ export class PathwayService {
 
   /**
    * Update a specific pathway
-   * @param WPId
+   * @param id
    * @param updates
    */
-  update(WPId: number, updates: {description: string}): Promise<any> {
-    return this.fb.db.ref('pathways/' + WPId).update(updates);
+  update(id: string, updates: {title: string, description: string}): Promise<any> {
+    return this.fb.db.ref('pathways/' + id).update(updates);
   }
 
   /**
    * Delete a pathway
-   * @param WPId
+   * @param id
    */
-  destroy(WPId: number): void {
-    this.fb.db.ref('pathways/' + WPId).remove();
+  destroy(id: string): void {
+    this.fb.db.ref('pathways/' + id).remove();
   }
 
   /**
    * List all the pathways in ascending order. Ordered by the value of the WPId
    * @param limit - number of entries to return
-   * @param startAt - The WPId to start at (inclusive)
+   * @param startAt - The id to start at (inclusive)
    * @returns {Observable}
    */
-  list(limit = 25, startAt?): Observable<{WPId: number, description: string, userId: string}[]> {
+  list(limit = 25, startAt?): Observable<{id: string, WPId: number, title: string, description: string, userId: string}[]> {
     return Observable.create(observer => {
       const ref = this.fb.db.ref('pathways').orderByKey().limitToFirst(limit);
       if (startAt) {
@@ -72,7 +75,9 @@ export class PathwayService {
           const val = data.val();
           returnVal.push(
             {
-              WPId: data.key(),
+              id: data.key(),
+              WPId: val.WPId,
+              title: val.title,
               description: val.description,
               userId: val.userId
             }
