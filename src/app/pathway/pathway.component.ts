e9b3ed0ee13391321af56fb77f43ac1bb7c2d9bd
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-07 01:09:51 +0200

    Allow zooming to groups

--- src/app/pathway/pathway.component.ts
+++ src/app/pathway/pathway.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import {PathwayService} from "../pathway.service";
 import * as Showdown from 'showdown';
+import {DomSanitizer} from "@angular/platform-browser";
 
 // TODO: Track https://github.com/furqanZafar/react-selectize/pull/130 and add back when can compile
 // import {Pvjs} from 'pvjs';
@@ -19,18 +20,41 @@ export class PathwayComponent implements OnInit {
   title: string;
   description: string;
 
-  constructor(private route: ActivatedRoute, public pathwayService: PathwayService) { }
+  constructor(private route: ActivatedRoute, public pathwayService: PathwayService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    const showdownExt = () => {
+      return [
+        {
+          type: 'lang',
+          filter: function (text, converter, options) {
+            // Replace zoomOn
+            text = text.replace(/\[(?:(?:zoomOn) ([\w, ?]+))]\((.+)\)/g, (match, p1, p2) => {
+              const nodes = p1.split(/\s*,\s*/);
+              let nodesAsString = '[';
+              nodes.forEach(node => {
+                nodesAsString += `'${node}',`;
+              });
+              nodesAsString += ']';
+              return `<a onclick="pathwayInstance.manipulator.zoomOn(${nodesAsString})">${p2}</a>`;
+            });
+            return text;
+          }
+        },
+      ];
+    };
+
+
     const converter = new Showdown.Converter({
-      headerLevelStart: 2
+      headerLevelStart: 2, // Set the header level to two since we have a title with h1
+      extensions: showdownExt()
     });
 
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
       this.pathwayService.get(id).subscribe(pathway => {
         this.title = pathway.title;
-        this.description = converter.makeHtml(pathway.description);
+        this.description = this.sanitizer.bypassSecurityTrustHtml(converter.makeHtml(pathway.description)) as string;
 
 
         Pvjs.loadDiagram('#pathway', 'WP' + pathway.WPId, {
@@ -38,7 +62,7 @@ export class PathwayComponent implements OnInit {
           height: '100%'
         }, instance => {
           this.pathwayInstance = instance;
-          window.instance = instance;
+          window.pathwayInstance = instance;
         });
       });
     });
