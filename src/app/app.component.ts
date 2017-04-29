00d200d921da09ee62783d37655e775379f9c073
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-29 17:23:23 +0200

    Bring back the search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import {AuthService} from "./auth.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(public auth: AuthService) { }
